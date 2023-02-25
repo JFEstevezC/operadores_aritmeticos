997cb7d597c5663d3997d0abf37879ce889600fa
JFEstevezC <jose2224646@correo.uis.edu.co>
2023-02-25 07:49:29 -0500

    Actualización 2 del programa

---
+++ operadores_aritmeticos.py
@@ -0,0 +1,28 @@
+# Programa para implementar los operadores aritméticos 
+print("-----------------------------------------------")
+print("------------OPERADORES ARITMËTICOS-------------")
+print("-----------------------------------------------")
+# input
+x = int(input("Digite el valor de x: "))
+y = int(input("Digite el valor de y: "))
+
+# processing
+s = x + y
+r = x - y
+m = x * y
+d = x / y
+mod = x % y
+de = x // y
+p = x ** y
+
+# output
+print("-----------------------------------------------")
+print("------------------ RESULTADOS -----------------")
+print("-----------------------------------------------")
+print("Suma: " + str(s))
+print("Resta: " + str(r))
+print("Multiplicación: " + str(m))
+print("División: " + str(d))
+print("Módulo: " + str(mod))
+print("División entera: " + str(de))
+print("Potencia: " + str(p))
